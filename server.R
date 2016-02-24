82d3775e4f3c65e49e8ddf4a75760b8e44b7edae
dreamingofdata <dreamingofdata@gmail.com>
2016-02-24 17:29:26 -0500

    Final shiny app deployed.

--- server.R
+++ server.R
@@ -1,5 +1,40 @@
 library(shiny)
+library(dplyr)
+sldata <- read.csv("data/SingleDaySL.csv")
+levels(sldata$ConceptName)[1] <- 'NONE'
+
 shinyServer(
   function(input, output) {
+    dataInput <- reactive({
+      summarizeBy <- input$summarizeBy
+      grouped_data <- sldata %>% group_by_(summarizeBy)
+      
+      metric <- switch(input$metric, 
+                     "Pieces Shipped" = grouped_data %>% summarize(value = sum(InvoicePieces, na.rm = TRUE)),
+                     "Pieces Damaged" = grouped_data %>% summarize(value = sum(Credit...Damage, na.rm = TRUE)),
+                     "Pieces Missing" = grouped_data %>% summarize(value = sum(Credit...Short, na.rm = TRUE)),
+                     "Pieces Mispicked" = grouped_data %>% summarize(value = sum(Credit...Mispick, na.rm = TRUE)))
+      names(metric) <- c('grouped_by', "value")
+      metric <- metric %>% 
+                top_n(10) %>%
+                arrange(-value) %>%
+                filter(value > 0)
+    })
+    
+    output$table <- renderTable(dataInput())
+    
+    output$barchart <- renderPlot({
+      data <- dataInput()
+      plotcolor <- switch(input$metric, 
+                          "Pieces Shipped" = "blue",
+                          "Pieces Damaged" = "red",
+                          "Pieces Missing" = "green",
+                          "Pieces Mispicked" = "yellow")
+      title <- paste(input$metric, "grouped by", input$summarizeBy)
+      barplot(data$value, 
+              names.arg=data$grouped_by,
+              col= plotcolor,
+              main = title,
+              las = 2)})
   }
 )
--- ui.R
+++ ui.R
@@ -1,10 +1,36 @@
 library(shiny)
-shinyUI(pageWithSidebar(
-  headerPanel("Data science FTW!"),
-  sidebarPanel(
-    h3('Sidebar text')
+
+shinyUI(fluidPage(
+  titlePanel('Service Level Explorer'),
+  fluidRow(
+    helpText("A distributor to restuarants monitors various performance metrics.",
+             "This application could be utilized to quickly navigate through ",
+             "various KPIs for different organizations throughout the country.")
   ),
-  mainPanel(
-    h3('Main Panel text')
+  sidebarLayout(
+    sidebarPanel(
+       h3('Choose inputs'),
+       selectInput("metric", "Choose Metric", 
+                   c("Pieces Shipped", "Pieces Damaged", 
+                  "Pieces Missing", "Pieces Mispicked"), 
+                   selected = "SalesOrg", multiple = FALSE),
+       selectInput("summarizeBy", "Summarize By", 
+                   c("State", "SalesOrg", "ConceptName"), 
+                   selected = "SalesOrg", multiple = FALSE),
+       helpText("View various metrics grouped in different",
+                "ways. Just select a desired metric and the",
+                "summarization type. The plot to the right",
+                "will update automatically!",
+                br(),br(),
+                "Note: For demonstration purposes only.",
+                "Data represents on a single day's worth",
+                "of deliveries.")
+       
+    ),
+    mainPanel(
+      plotOutput("barchart")
+    )
+
   )
 ))
+
